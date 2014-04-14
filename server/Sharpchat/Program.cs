31b7ac4ce5f6a8858dd2dd1461cbab4b514f67c9
vegax87 <vegax987@gmail.com>
2014-04-14 13:28:42 +0100

    Better connection Info

--- server/Sharpchat/Program.cs
+++ server/Sharpchat/Program.cs
@@ -10,27 +10,40 @@ namespace Websocket
     {
         static void Main(string[] args)
         {
-            FleckLog.Level = LogLevel.Debug;
+            FleckLog.Level = LogLevel.Info;
             var allsockets = new List<IWebSocketConnection>();
             var server = new WebSocketServer("ws://localhost:8181");
 
             server.Start(socket =>
             {
                 socket.OnOpen = () =>
-                    {
-                        Console.WriteLine("Open");
+                    {   //See socket.ConnectionInfo.* for additional informations
+                        Console.WriteLine(String.Empty);
+                        Console.WriteLine("[NEW CLIENT CONNECTION]======================");
+                        Console.WriteLine("GUID: " + socket.ConnectionInfo.Id);
+                        Console.WriteLine("IP: " + socket.ConnectionInfo.ClientIpAddress);
+                        Console.WriteLine("Port: " + socket.ConnectionInfo.ClientPort);
+                        Console.WriteLine("=============================================");
+                        Console.WriteLine(String.Empty);
                         allsockets.Add(socket);
 
                     };
 
                 socket.OnClose = () =>
-                    {
-                        Console.WriteLine("Closed connection");
+                {
+                        Console.WriteLine(String.Empty);
+                        Console.WriteLine("[DISCONNECTED CLIENT]=======================");
+                        Console.WriteLine("GUID: " + socket.ConnectionInfo.Id);
+                        Console.WriteLine("IP: " + socket.ConnectionInfo.ClientIpAddress);
+                        Console.WriteLine("Port: " + socket.ConnectionInfo.ClientPort);
+                        Console.WriteLine("=============================================");
+                        Console.WriteLine(String.Empty);
                         allsockets.Remove(socket);
                     };
 
                 socket.OnMessage = (message) =>
                 {
+                    //TODO: Json.Net Deserialize
                     Console.WriteLine("[JSON MESSAGE] " + message);
                     allsockets.ToList().ForEach(s => s.Send(message));
                 };
